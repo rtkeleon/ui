dae0173b0057e51f93f5e1c284d38537ef10100e
Puneet Ghuman <puneet.ghuman@arcticwolf.com>
2020-11-11 09:50:35 -0500

    change tests

--- src/components/markdown/Markdown.tsx
+++ src/components/markdown/Markdown.tsx
@@ -1,27 +1,19 @@
 import * as React from 'react';
-import * as MarkdownIt from 'markdown-it';
 
 type Props = {
   children?: React.ReactNode;
 };
 
-/*
-  Adding this as a simpler way to manage markdown wrt to the current
-  implementations found in MarkdownRenderer this implementation doesnt
-  bake in any additional functionality. You simply pass it a string as children
-  and you get markdown.
-*/
-export const Markdown = ({ children }: Props) => {
-  const md = React.useRef(new MarkdownIt({}));
+declare function require(name: string);
 
+export const Markdown = ({ children }: Props) => {
+  const md = require('markdown-it')();
   return (
     <div className="markdown">
       {React.Children.map(children, child => {
         if (typeof child === 'string') {
           return (
-            <span
-              dangerouslySetInnerHTML={{ __html: md.current.render(child) }}
-            />
+            <span dangerouslySetInnerHTML={{ __html: md.render(child) }} />
           );
         } else {
           return child;
--- src/components/markdown/__tests__/Markdown.spec.tsx
+++ src/components/markdown/__tests__/Markdown.spec.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
 
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import { Markdown } from '../Markdown';
 
-describe('Markdown', () => {
-  it('renders', () => {
-    const wrapper = shallow(<Markdown>{'This is test'}</Markdown>);
+const MD_TEXT = `
+# Title
 
-    expect(wrapper.contains('This is test')).toExist();
+- some list
+- of stuff
+`;
+
+describe('Markdown', () => {
+  it('renders child text nodes as HTML', () => {
+    const html = shallow(<Markdown>{MD_TEXT}</Markdown>).render();
+    expect(html.find('h1')).toHaveLength(1);
+    expect(html.find('li')).toHaveLength(2);
+  });
+
+  it('renders non-text children unchanged', () => {
+    const wrapper = shallow(
+      <Markdown>
+        <div>This is test</div>
+      </Markdown>
+    );
+    expect(wrapper.childAt(0).type()).toEqual('div');
+    expect(wrapper.render().find('h1')).toHaveLength(0);
   });
 });
