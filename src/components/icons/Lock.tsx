675c68d08eb1b7006804bfcb92d14384b9fc609a
Emilio Leon <emilio.leon@arcticwolf.com>
2020-11-16 17:50:06 -0500

    Added lock icon

---
+++ src/components/icons/Lock.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faLock } from '@fortawesome/free-solid-svg-icons/faLock';
+
+import { IconProps } from './types';
+
+const Lock: React.FunctionComponent<IconProps> = props => {
+  return <FontAwesomeIcon icon={faLock} {...props} />;
+};
+
+export default Lock;
